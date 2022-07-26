6cd60a52d0663a807e9ba20f3e154b25a165b960
nenadvasilic <vasilic2013@gmail.com>
2022-07-26 19:36:03 +0200

    commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Birthday Reminder React App
+
+## Getting started
+
+```js
+npm i && npm start
+//run tests
+npm run wdio
+```
+
+React Testing Course for Beginners – Code and Test 3 Apps (2nd App)
---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import data from "./data";
+import List from "./List";
+
+function App() {
+  const [people, setPeople] = useState(data);
+
+  return (
+    <main>
+      <section className="container">
+        <h1>{people.length} birthdays</h1>
+        <List people={people}></List>
+        <button data-testid="clear" onClick={() => setPeople([])}>
+          clear all
+        </button>
+      </section>
+    </main>
+  );
+}
+
+export default App;
